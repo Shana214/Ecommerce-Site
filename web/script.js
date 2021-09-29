474bf04705d8a6166d1df346c8a6f98e6c1b106e
Shana214 <90228803+Shana214@users.noreply.github.com>
2021-09-29 15:29:18 +0800

    Delete web directory

--- web/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load' , () -> {
-	const params = (new( URL(document.location)).searchParams;
-	const name = params.get('product-name');
-	
-	document.getElementById('page-title').innerHTML = name;
-})
